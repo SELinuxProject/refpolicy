7c347cddcb54966ddab3e4b97b0b384c360f72da
Chris PeBenito <cpebenito@tresys.com>
2005-08-01 17:55:04 +0000

    more updates for release

--- www/publish.sh
+++ www/publish.sh
@@ -3,4 +3,4 @@
 # This will 'publish' the reference policy website.
 
 cp ../refpolicy/Changelog html/Changelog.txt
-rsync -r . shell.sf.net:/home/groups/s/se/serefpolicy/htdocs
+rsync -avr --exclude .svn . shell.sf.net:/home/groups/s/se/serefpolicy/htdocs
