af7ca5000e762a8fe5e106ba568cebab19aef0ce
Chris PeBenito <cpebenito@tresys.com>
2006-07-06 17:59:10 +0000

    remove cruft from tags

--- libselinux/src/get_default_type_internal.h
+++
@@ -1,4 +0,0 @@
-#include <selinux/get_default_type.h>
-#include "dso.h"
-
-hidden_proto(selinux_default_type_path)
--- libselinux/utils/selinuxenabled.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <selinux/selinux.h>
-
-int main(void) 
-{
-	return !is_selinux_enabled();
-}
