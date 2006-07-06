385e62440756f4c43958e11fe27104b48c7aa322
Chris PeBenito <cpebenito@tresys.com>
2006-07-06 17:35:17 +0000

    move non-policy dirs out of trunk

--- www/publish.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-# This will 'publish' the reference policy website.
-
-#cp ../refpolicy/Changelog html/Changelog.txt
-rsync -rv --exclude .svn . shell.sf.net:/home/groups/s/se/serefpolicy/htdocs
